304f3d06054b2c626573aca6c059be5f4f94c16b
komukomo <komukomo@users.noreply.github.com>
2016-12-07 11:53:40 +0900

    Read disk

--- helloos.s
+++ helloos.s
@@ -1,5 +1,6 @@
-    .code16
+.equ CYLS, 10
 .text
+.code16
     jmp entry
 
     .byte 0x90
@@ -28,6 +29,50 @@ entry:
     movw $0x7c00, %sp
     movw %ax, %ds
     movw %ax, %es
+
+    movw $0x0820, %ax
+    movw %ax, %es
+    movb $0x00, %ch # cylinder:0
+    movb $0x00, %dh # head:0
+    movb $0x02, %cl # sector:2
+readloop:
+    movw $0x00, %si
+retry:
+    movb $0x02, %ah # 0x02:Read Sectors
+    movb $0x01, %al # 1 sector
+    movw $0, %bx
+    movb $0x00, %dl # drive:0
+    int $0x13 # 0x13:Disk Services
+    jnc next
+    addw $0x01, %si
+    cmpw $0x05, %si
+    jae error
+    movb $0x00, %ah
+    movb $0x00, %dl
+    int $0x13 # reset
+    jmp retry
+next:
+    movw %es, %ax
+    addw $0x0020, %ax
+    movw %ax, %es
+    addb $0x01, %cl
+    cmpb $18, %cl
+    jbe readloop
+
+    movb $0x01, %cl
+    addb $0x01, %dh
+    cmpb $0x02, %dh
+    jb readloop
+    movb $0x00, %dh
+    addb $0x01, %ch
+    cmpb $CYLS, %ch
+    jb readloop
+
+fin:
+    hlt
+    jmp fin
+
+error:
     movw $msg, %si
 putloop:
     movb (%si), %al
@@ -38,9 +83,6 @@ putloop:
     movw $15, %bx
     int $0x10
     jmp putloop
-fin:
-    hlt
-    jmp fin
 msg:
     .byte 0x0a, 0x0a
-    .string "hello, world"
+    .string "load error"
