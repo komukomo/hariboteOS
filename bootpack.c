b93ddaf9ad75a8199f75ac70a1de048a219fe96e
komukomo <komukomo@users.noreply.github.com>
2016-12-09 20:56:26 +0900

    Add mouse cursor

--- bootpack.c
+++ bootpack.c
@@ -7,6 +7,8 @@ void boxfill8(unsigned char *, int, unsigned char, int, int, int, int);
 void init_screen(char *, int, int);
 void putfont8(char *, int, int, int, char, char *);
 void putfonts8_asc(char *, int, int, int, char, unsigned char *);
+void init_mouse_cursor8(char *, char);
+void putblock8_8(char *, int, int, int, int, int, char *, int);
 
 #define COL8_000000 0
 #define COL8_FF0000 1
@@ -37,7 +39,9 @@ struct BOOTINFO {
 
 void Main(void) {
   struct BOOTINFO *binfo = (struct BOOTINFO *)0x0ff0;
-  char s[40];
+  char s[40], mcursor[256];
+  int my = (binfo->scrny - 16) / 2;
+  int mx = (binfo->scrnx - 28 - 16) / 2;
 
   init_palette();
   init_screen(binfo->vram, binfo->scrnx, binfo->scrny);
@@ -45,6 +49,8 @@ void Main(void) {
   mysprintf(s, "(%d, %d) %d", binfo->scrnx, binfo->scrny, 4);
   putfonts8_asc(binfo->vram, binfo->scrnx, 8, 32, COL8_000000, s);
   putfonts8_asc(binfo->vram, binfo->scrnx, 8, 8, COL8_000000, "ABC 123.");
+  init_mouse_cursor8(mcursor, COL8_008484);
+  putblock8_8(binfo->vram, binfo->scrnx, 16, 16, mx, my, mcursor, 16);
 
   for (;;) {
     io_hlt();
@@ -146,6 +152,43 @@ void putfonts8_asc(char *vram, int xsize, int x, int y, char c,
   for (; *s != 0x00; s++) {
     putfont8(vram, xsize, x, y, c, hankaku + *s * 16);
     x += 8;
+  }
+  return;
+}
+
+void init_mouse_cursor8(char *mouse, char bc) {
+  static char cursor[16][16] = {
+      "**************..", "*OOOOOOOOOOO*...", "*OOOOOOOOOO*....",
+      "*OOOOOOOOO*.....", "*OOOOOOOO*......", "*OOOOOOO*.......",
+      "*OOOOOOO*.......", "*OOOOOOOO*......", "*OOOO**OOO*.....",
+      "*OOO*..*OOO*....", "*OO*....*OOO*...", "*O*......*OOO*..",
+      "**........*OOO*.", "*..........*OOO*", "............*OO*",
+      ".............***"};
+  int x, y;
+
+  for (y = 0; y < 16; y++) {
+    for (x = 0; x < 16; x++) {
+      if (cursor[y][x] == '*') {
+        mouse[y * 16 + x] = COL8_000000;
+      }
+      if (cursor[y][x] == 'O') {
+        mouse[y * 16 + x] = COL8_FFFFFF;
+      }
+      if (cursor[y][x] == '.') {
+        mouse[y * 16 + x] = bc;
+      }
+    }
+  }
+  return;
+}
+
+void putblock8_8(char *vram, int vxsize, int pxsize, int pysize, int px0,
+                 int py0, char *buf, int bxsize) {
+  int x, y;
+  for (y = 0; y < pysize; y++) {
+    for (x = 0; x < pxsize; x++) {
+      vram[(py0 + y) * vxsize + (px0 + x)] = buf[y * bxsize + x];
+    }
   }
   return;
 }
