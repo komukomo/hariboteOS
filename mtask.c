658d31a2170f6f9a08e7114141ac8fc8499965fa
komukomo <komukomo@users.noreply.github.com>
2016-12-28 01:14:07 +0900

    Set priorities on task

--- mtask.c
+++ mtask.c
@@ -15,13 +15,14 @@ struct TASK *task_init(struct MEMMAN *memman) {
                  AR_TSS32);
   }
   task = task_alloc();
-  task->flags = 2; /* 動作中マーク */
+  task->flags = 2;    /* 動作中マーク */
+  task->priority = 2; /* 0.02秒 */
   taskctl->running = 1;
   taskctl->now = 0;
   taskctl->tasks[0] = task;
   load_tr(task->sel);
   task_timer = timer_alloc();
-  timer_settime(task_timer, 2);
+  timer_settime(task_timer, task->priority);
   return task;
 }
 
@@ -52,21 +53,28 @@ struct TASK *task_alloc(void) {
   return 0; /* もう全部使用中 */
 }
 
-void task_run(struct TASK *task) {
-  task->flags = 2; /* 動作中マーク */
-  taskctl->tasks[taskctl->running] = task;
-  taskctl->running++;
+void task_run(struct TASK *task, int priority) {
+  if (priority > 0) {
+    task->priority = priority;
+  }
+  if (task->flags != 2) {
+    task->flags = 2; /* 動作中マーク */
+    taskctl->tasks[taskctl->running] = task;
+    taskctl->running++;
+  }
   return;
 }
 
 void task_switch(void) {
-  timer_settime(task_timer, 2);
+  struct TASK *task;
+  taskctl->now++;
+  if (taskctl->now == taskctl->running) {
+    taskctl->now = 0;
+  }
+  task = taskctl->tasks[taskctl->now];
+  timer_settime(task_timer, task->priority);
   if (taskctl->running >= 2) {
-    taskctl->now++;
-    if (taskctl->now == taskctl->running) {
-      taskctl->now = 0;
-    }
-    farjmp(0, taskctl->tasks[taskctl->now]->sel);
+    farjmp(0, task->sel);
   }
   return;
 }
