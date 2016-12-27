245240c51a36ab21a3de736f9b2a756d1b7dee28
komukomo <komukomo@users.noreply.github.com>
2016-12-28 00:42:09 +0900

    Add task_sleep

--- bootpack.h
+++ bootpack.h
@@ -37,8 +37,9 @@ void mysprintf(char *dest, const char *string, ...);
 struct FIFO32 {
   int *buf;
   int p, q, size, free, flags;
+  struct TASK *task;
 };
-void fifo32_init(struct FIFO32 *fifo, int size, int *buf);
+void fifo32_init(struct FIFO32 *fifo, int size, int *buf, struct TASK *task);
 int fifo32_put(struct FIFO32 *fifo, int data);
 int fifo32_get(struct FIFO32 *fifo);
 int fifo32_status(struct FIFO32 *fifo);
