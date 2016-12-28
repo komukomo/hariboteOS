ba18cbcd0c3893a0e234c244db9a880f4eab0555
komukomo <komukomo@users.noreply.github.com>
2016-12-28 16:59:49 +0900

    Enable upper cases

--- bootpack.c
+++ bootpack.c
@@ -44,7 +44,7 @@ void Main(void) {
       0,   0,   0,   0,    0,   0,   0,   0,   0,    0,   0,   0,   0,
       0,   0,   0,   0,    0,   0,   0,   0,   0,    0,   0,   '_', 0,
       0,   0,   0,   0,    0,   0,   0,   0,   '|',  0,   0};
-  int key_to = 0, key_shift = 0;
+  int key_to = 0, key_shift = 0, key_leds = (binfo->leds >> 4) & 7;
 
   init_gdtidt();
   init_pic();
@@ -145,6 +145,12 @@ void Main(void) {
           }
         } else {
           s[0] = 0;
+        }
+        if ('A' <= s[0] && s[0] <= 'Z') { /* 入力文字がアルファベット */
+          if (((key_leds & 4) == 0 && key_shift == 0) ||
+              ((key_leds & 4) != 0 && key_shift != 0)) {
+            s[0] += 0x20; /* 大文字を小文字に変換 */
+          }
         }
         if (s[0] != 0) {     /* 通常文字 */
           if (key_to == 0) { /* タスクAへ */
