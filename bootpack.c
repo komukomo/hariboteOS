31599e16f39c775280fe257bb9080f41cd44cf86
komukomo <komukomo@users.noreply.github.com>
2016-12-28 14:29:17 +0900

    Split make_window

--- bootpack.c
+++ bootpack.c
@@ -196,6 +196,20 @@ void Main(void) {
 
 void make_window8(unsigned char *buf, int xsize, int ysize, char *title,
                   char act) {
+  boxfill8(buf, xsize, COL8_C6C6C6, 0, 0, xsize - 1, 0);
+  boxfill8(buf, xsize, COL8_FFFFFF, 1, 1, xsize - 2, 1);
+  boxfill8(buf, xsize, COL8_C6C6C6, 0, 0, 0, ysize - 1);
+  boxfill8(buf, xsize, COL8_FFFFFF, 1, 1, 1, ysize - 2);
+  boxfill8(buf, xsize, COL8_848484, xsize - 2, 1, xsize - 2, ysize - 2);
+  boxfill8(buf, xsize, COL8_000000, xsize - 1, 0, xsize - 1, ysize - 1);
+  boxfill8(buf, xsize, COL8_C6C6C6, 2, 2, xsize - 3, ysize - 3);
+  boxfill8(buf, xsize, COL8_848484, 1, ysize - 2, xsize - 2, ysize - 2);
+  boxfill8(buf, xsize, COL8_000000, 0, ysize - 1, xsize - 1, ysize - 1);
+  make_wtitle8(buf, xsize, title, act);
+  return;
+}
+
+void make_wtitle8(unsigned char *buf, int xsize, char *title, char act) {
   static char closebtn[14][16] = {
       "OOOOOOOOOOOOOOO@", "OQQQQQQQQQQQQQ$@", "OQQQQQQQQQQQQQ$@",
       "OQQQ@@QQQQ@@QQ$@", "OQQQQ@@QQ@@QQQ$@", "OQQQQQ@@@@QQQQ$@",
@@ -211,16 +225,7 @@ void make_window8(unsigned char *buf, int xsize, int ysize, char *title,
     tc = COL8_C6C6C6;
     tbc = COL8_848484;
   }
-  boxfill8(buf, xsize, COL8_C6C6C6, 0, 0, xsize - 1, 0);
-  boxfill8(buf, xsize, COL8_FFFFFF, 1, 1, xsize - 2, 1);
-  boxfill8(buf, xsize, COL8_C6C6C6, 0, 0, 0, ysize - 1);
-  boxfill8(buf, xsize, COL8_FFFFFF, 1, 1, 1, ysize - 2);
-  boxfill8(buf, xsize, COL8_848484, xsize - 2, 1, xsize - 2, ysize - 2);
-  boxfill8(buf, xsize, COL8_000000, xsize - 1, 0, xsize - 1, ysize - 1);
-  boxfill8(buf, xsize, COL8_C6C6C6, 2, 2, xsize - 3, ysize - 3);
   boxfill8(buf, xsize, tbc, 3, 3, xsize - 4, 20);
-  boxfill8(buf, xsize, COL8_848484, 1, ysize - 2, xsize - 2, ysize - 2);
-  boxfill8(buf, xsize, COL8_000000, 0, ysize - 1, xsize - 1, ysize - 1);
   putfonts8_asc(buf, xsize, 24, 4, tc, title);
   for (y = 0; y < 14; y++) {
     for (x = 0; x < 16; x++) {
