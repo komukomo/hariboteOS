0e699f7bbe5eeace486a92310d3ccd6c90f433c9
komukomo <komukomo@users.noreply.github.com>
2016-12-31 23:19:10 +0900

    Add ncst command
    "ncst" means "no console start"

--- bootpack.h
+++ bootpack.h
@@ -240,6 +240,7 @@ struct TASKCTL {
   struct TASKLEVEL level[MAX_TASKLEVELS];
   struct TASK tasks0[MAX_TASKS];
 };
+extern struct TASKCTL *taskctl;
 extern struct TIMER *task_timer;
 struct TASK *task_now(void);
 struct TASK *task_init(struct MEMMAN *memman);
@@ -274,6 +275,8 @@ void cmd_cls(struct CONSOLE *cons);
 void cmd_dir(struct CONSOLE *cons);
 void cmd_type(struct CONSOLE *cons, int *fat, char *cmdline);
 void cmd_exit(struct CONSOLE *cons, int *fat);
+void cmd_start(struct CONSOLE *cons, char *cmdline, int memtotal);
+void cmd_ncst(struct CONSOLE *cons, char *cmdline, int memtotal);
 int cmd_app(struct CONSOLE *cons, int *fat, char *cmdline);
 int *hrb_api(int edi, int esi, int ebp, int esp, int ebx, int edx, int ecx,
              int eax);
@@ -281,7 +284,6 @@ int *inthandler0d(int *esp);
 int *inthandler0c(int *esp);
 void hrb_api_linewin(struct SHEET *sht, int x0, int y0, int x1, int y1,
                      int col);
-
 // file.c
 struct FILEINFO {
   unsigned char name[8], ext[3], type;
@@ -294,4 +296,5 @@ void file_loadfile(int clustno, int size, char *buf, int *fat, char *img);
 struct FILEINFO *file_search(char *name, struct FILEINFO *finfo, int max);
 
 // bootpack.c
+struct TASK *open_constask(struct SHEET *sht, unsigned int memtotal);
 struct SHEET *open_console(struct SHTCTL *shtctl, unsigned int memtotal);
