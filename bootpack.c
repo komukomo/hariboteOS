556425b4cbe6931a135a2b7ee3f5938a52bf6fbf
komukomo <komukomo@users.noreply.github.com>
2016-12-31 21:08:39 +0900

    Raise the console window by F11

--- bootpack.c
+++ bootpack.c
@@ -239,6 +239,9 @@ void Main(void) {
           task_cons->tss.eip = (int)asm_end_app;
           io_sti();
         }
+        if (i == 256 + 0x57 && shtctl->top > 2) { /* F11 */
+          sheet_updown(shtctl->sheets[1], shtctl->top - 1);
+        }
         if (i == 256 + 0xfa) { /* キーボードがデータを無事に受け取った */
           keycmd_wait = -1;
         }
