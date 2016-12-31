ae2a9e364ee7b729bd4d7996d115edf76f78d648
komukomo <komukomo@users.noreply.github.com>
2016-12-31 21:01:20 +0900

    Close the window at force quit

--- bootpack.h
+++ bootpack.h
@@ -168,6 +168,7 @@ struct SHEET {
   unsigned char *buf;
   int bxsize, bysize, vx0, vy0, col_inv, height, flags;
   struct SHTCTL *ctl;
+  struct TASK *task;
 };
 struct SHTCTL {
   unsigned char *vram, *map;
