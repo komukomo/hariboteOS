dc9491134d131fc7257b144304cd7cfcfe91375d
komukomo <komukomo@users.noreply.github.com>
2016-12-31 23:17:51 +0900

    fix

--- color.c
+++ color.c
@@ -1,3 +1,5 @@
+int api_openwin(char *buf, int xsiz, int ysiz, int col_inv, char *title);
+void api_initmalloc(void);
 char *api_malloc(int size);
 void api_refreshwin(int win, int x0, int y0, int x1, int y1);
 void api_linewin(int win, int x0, int y0, int x1, int y1, int col);
