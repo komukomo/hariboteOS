d8775e0da0d788cf462a68d40c15b2498dfb1064
komukomo <komukomo@users.noreply.github.com>
2016-12-31 16:27:26 +0900

    Add draw lines API

--- Makefile
+++ Makefile
@@ -8,7 +8,7 @@ LDFLAGS = -m elf_i386
 AS = as
 ASFLAGS = --32
 
-$(img): ipl.bin head.bin bootpack.bin hello.bin hello2.bin a.hrb hello4.hrb winhelo.hrb winhelo2.hrb winhelo3.hrb star1.hrb crack1.hrb
+$(img): ipl.bin head.bin bootpack.bin hello.bin hello2.bin a.hrb hello4.hrb winhelo.hrb winhelo2.hrb winhelo3.hrb star1.hrb lines.hrb crack1.hrb
 	cat head.bin bootpack.bin > sys.bin
 	mformat -f 1440 -C -B ipl.bin -i $@
 	mcopy sys.bin -i $@ ::
@@ -21,6 +21,7 @@ $(img): ipl.bin head.bin bootpack.bin hello.bin hello2.bin a.hrb hello4.hrb winh
 	mcopy winhelo2.hrb -i $@ ::
 	mcopy winhelo3.hrb -i $@ ::
 	mcopy star1.hrb -i $@ ::
+	mcopy lines.hrb -i $@ ::
 	mcopy crack1.hrb -i $@ ::
 
 bootpack.bin: $(objs) bootpack.ld
--- api.s
+++ api.s
@@ -2,6 +2,7 @@
 .global api_putstrwin, api_boxfilwin
 .global api_initmalloc, api_malloc, api_free
 .global api_point
+.global api_refreshwin, api_linewin
 .extern Main
 .text
     .long 0x10000 # segment size
@@ -156,6 +157,26 @@ api_refreshwin:
     mov  32(%esp), %edi # y1
     int  $0x40
     pop  %ebx
+    pop  %esi
+    pop  %edi
+    ret
+
+# void api_linewin(int win, int x0, int y0, int x1, int y1, int col);
+api_linewin:
+    push %edi
+    push %esi
+    push %ebp
+    push %ebx
+    mov  $13, %edx
+    mov  20(%esp), %ebx # win
+    mov  24(%esp), %eax # x0
+    mov  28(%esp), %ecx # y0
+    mov  32(%esp), %esi # x1
+    mov  36(%esp), %edi # y1
+    mov  40(%esp), %ebp # col
+    int  $0x40
+    pop  %ebx
+    pop  %ebp
     pop  %esi
     pop  %edi
     ret
