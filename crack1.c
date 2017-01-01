113f11cf5a9a636d323fe0f59255b0fd8758f934
komukomo <komukomo@users.noreply.github.com>
2017-01-01 17:41:24 +0900

    Delete unnecessary files

--- crack1.c
+++
@@ -1,4 +0,0 @@
-void Main(void) {
-  *((char *)0x00102600) = 0;
-  return;
-}
--- hello2.s
+++
@@ -1,8 +0,0 @@
-    mov  $2, %edx
-    mov $msg, %ebx
-    int  $0x40
-    mov $4, %edx
-    int $0x40
-
-msg:
-    .ascii "hello2\0"
